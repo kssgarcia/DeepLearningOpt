68a1b7399ae8dda69c5991a2be19d85789795554
kssgarcia <kssgarcia@outlook.com>
2023-08-11 08:44:30 -0500

    Doc

--- simp/SIMP.py
+++ simp/SIMP.py
@@ -1,6 +1,5 @@
 # %%
 import time
-import logging
 import numpy as np
 from scipy.sparse.linalg import spsolve
 import solidspy.assemutil as ass # Solidspy 1.1.0
@@ -17,61 +16,53 @@ start_time = time.time()
 np.seterr(divide='ignore', invalid='ignore')
 
 # Initialize variables
-length = 10
-height = 10
+length = 60
+height = 60
 nx = 60
 ny= 60
 niter = 60
-penal = 3
-Emin=1e-9
-Emax=1.0
+penal = 3 # Penalization factor
+Emin=1e-9 # Minimum young modulus of the material
+Emax=1.0 # Maximum young modulus of the material
 
-# Initialize storage variables
-input_load = []
-input_bc = []
-ouput_rho = []
-
-a = True
 iter = 0
-r = 30
-c = 10
-volfrac = 0.6
-load = np.zeros((nx+1, ny+1), dtype=int)
-load[-r, -c] = 1
+r = 60
+c = 1
+volfrac = 0.4 # Volume fraction
 iter += 1
-node_index = nx*r+(r-c)
+node_index = nx*r+(r-c) # Change the linear 
 nodes, mats, els, loads = beam(L=length, H=height, nx=nx, ny=ny, n=node_index)
 print(loads)
 
 # Initialize the design variables
-change = 10
-g = 0
-rho = volfrac * np.ones(ny*nx, dtype=float)
-sensi_rho = np.ones(ny*nx)
-rho_old = rho.copy()
-d_c = np.ones(ny*nx)
+change = 10 # Change in the design variable
+g = 0 # Constraint
+rho = volfrac * np.ones(ny*nx, dtype=float) # Initialize the density
+sensi_rho = np.ones(ny*nx) # Initialize the sensitivity
+rho_old = rho.copy() # Initialize the density history
+d_c = np.ones(ny*nx) # Initialize the design change
+
 
 # Create and initialize channels
 bc = np.ones((nx + 1, ny + 1)) * volfrac
 bc[:, 0] = 1
+load = np.zeros((nx+1, ny+1), dtype=int)
+load[-r, -c] = 1
 
-if a:
-    r_min = np.linalg.norm(nodes[0,1:3] - nodes[1,1:3]) * 4
-    centers = center_els(nodes, els)
-    E = mats[0,0]
-    nu = mats[0,1]
-    k = np.array([1/2-nu/6,1/8+nu/8,-1/4-nu/12,-1/8+3*nu/8,-1/4+nu/12,-1/8-nu/8,nu/6,1/8-3*nu/8])
-    kloc = E/(1-nu**2)*np.array([ [k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7]],
-    [k[1], k[0], k[7], k[6], k[5], k[4], k[3], k[2]],
-    [k[2], k[7], k[0], k[5], k[6], k[3], k[4], k[1]],
-    [k[3], k[6], k[5], k[0], k[7], k[2], k[1], k[4]],
-    [k[4], k[5], k[6], k[7], k[0], k[1], k[2], k[3]],
-    [k[5], k[4], k[3], k[2], k[1], k[0], k[7], k[6]],
-    [k[6], k[3], k[4], k[1], k[2], k[7], k[0], k[5]],
-    [k[7], k[2], k[1], k[4], k[3], k[6], k[5], k[0]]]);
-    assem_op, bc_array, neq = ass.DME(nodes[:, -2:], els, ndof_el_max=8)
-
-a = False
+r_min = np.linalg.norm(nodes[0,1:3] - nodes[1,1:3]) * 4 # Radius for the sensitivity filter
+centers = center_els(nodes, els) # Calculate centers
+E = mats[0,0] # Young modulus
+nu = mats[0,1] # Poisson ratio
+k = np.array([1/2-nu/6,1/8+nu/8,-1/4-nu/12,-1/8+3*nu/8,-1/4+nu/12,-1/8-nu/8,nu/6,1/8-3*nu/8]) # Coefficients
+kloc = E/(1-nu**2)*np.array([ [k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7]], 
+[k[1], k[0], k[7], k[6], k[5], k[4], k[3], k[2]],
+[k[2], k[7], k[0], k[5], k[6], k[3], k[4], k[1]],
+[k[3], k[6], k[5], k[0], k[7], k[2], k[1], k[4]],
+[k[4], k[5], k[6], k[7], k[0], k[1], k[2], k[3]],
+[k[5], k[4], k[3], k[2], k[1], k[0], k[7], k[6]],
+[k[6], k[3], k[4], k[1], k[2], k[7], k[0], k[5]],
+[k[7], k[2], k[1], k[4], k[3], k[6], k[5], k[0]]]); # Local stiffness matrix
+assem_op, bc_array, neq = ass.DME(nodes[:, -2:], els, ndof_el_max=8) 
 
 for _ in range(niter):
 
