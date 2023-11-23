a388fce91e5b8bcb05af829b30a9319df57c1952
kssgarcia <kssgarcia@outlook.com>
2023-11-23 09:38:27 -0500

    New FEM data sample

--- simp/SIMP_multi_FEM.py
+++ simp/SIMP_multi_FEM.py
@@ -1,39 +1,30 @@
-# %%
+from os import path, makedirs
+import multiprocessing
+import logging
 import time
 import numpy as np
 from scipy.sparse.linalg import spsolve
 import solidspy.assemutil as ass # Solidspy 1.1.0
-
-import matplotlib.pyplot as plt 
-from matplotlib import colors
-
 from Utils.beams import *
 from Utils.SIMP_utils import *
 
-# Start the timer
-start_time = time.time()
-
 np.seterr(divide='ignore', invalid='ignore')
 
+# Initialize variables
+length = 10
+height = 10
+nx = 60
+ny= 60
+niter = 100
+penal = 3
+Emin=1e-9
+Emax=1.0
 
-def optimization(n_elem, r1, r2, volfrac):
-    # Initialize variables
-    length = 60
-    height = 60
-    nx = n_elem
-    ny= n_elem
-    niter = 60
-    penal = 3 # Penalization factor
-    Emin=1e-9 # Minimum young modulus of the material
-    Emax=1.0 # Maximum young modulus of the material
-
-    iter = 0
-    iter += 1
-    c = 1
-    node_index1 = nx*r1+(r1-c) # Change the linear 
-    node_index2 = nx*r2+(r2-c) # Change the linear 
+# Optimise function
+def optimise(r1, c1, r2, c2, volfrac):
+    node_index1 = nx*r1+(r1-c1) # Change the linear 
+    node_index2 = nx*r2+(r2-c2) # Change the linear 
     nodes, mats, els, loads = beam(L=length, H=height, nx=nx, ny=ny, n1=node_index1, n2=node_index2)
-    print(loads)
 
     # Initialize the design variables
     change = 10 # Change in the design variable
@@ -77,7 +68,7 @@ def optimization(n_elem, r1, r2, volfrac):
         # System solution
         disp = spsolve(stiff_mat, rhs_vec)
         UC = pos.complete_disp(bc_array, nodes, disp)
-        #strain_nodes, stress_nodes = pos.strain_nodes(nodes, els, mats[:,:2], UC)
+        _, stress_nodes = pos.strain_nodes(nodes, els, mats[:,:2], UC)
 
         # Sensitivity analysis
         sensi_rho[:] = (np.dot(UC[els[:,-4:]].reshape(nx*ny,8),kloc) * UC[els[:,-4:]].reshape(nx*ny,8) ).sum(1)
@@ -91,12 +82,64 @@ def optimization(n_elem, r1, r2, volfrac):
         # Compute the change
         change = np.linalg.norm(rho.reshape(nx*ny,1)-rho_old.reshape(nx*ny,1),np.inf)
 
-
-    plt.ion() 
-    fig,ax = plt.subplots()
-    ax.imshow(-rho.reshape(n_elem,n_elem), cmap='gray', interpolation='none',norm=colors.Normalize(vmin=-1,vmax=0))
-    ax.set_title('Predicted')
-    fig.show()
+    vol = np.ones((nx + 1, ny + 1)) * volfrac
+    return UC, vol.flatten(), stress_nodes, rho
 
 if __name__ == "__main__":
-    optimization(60, 60, 1, 0.4)
+    start_time = time.perf_counter()
+
+    # Initialize storage variables
+    input_bc = []
+    input_load = []
+    output_rho = []
+    tasks = []
+    results = []
+
+    # Create tasks
+    a = True
+    iter = 0
+    for l in [1,-1]:
+        for c1 in range(1, 2):
+            for r1 in range(1, 2):
+                for c2 in range(1, 2):
+                    for r2 in range(1, 2):
+                        for volfrac in [0.5,0.6,0.7,0.8,0.9]:
+                            iter += 1
+                            task = (r1, c1, r2, c2, volfrac)
+                            tasks.append(task)
+
+    # Create pool
+    num_processor = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_processor)
+    print(num_processor)
+
+    results = pool.starmap(optimise, tasks)
+    pool.close()
+    pool.join()
+
+    final_input_uc = []
+    final_input_vol = []
+    final_input_stress = []
+    final_output_rho = []
+
+    # Unpack results
+    for result in results:
+        final_input_uc.append(result[0])
+        final_input_vol.append(result[1])
+        final_input_stress.append(result[2])
+        final_output_rho.append(result[3])
+
+    # Save data
+    dir = './results'
+    if not path.exists(dir): makedirs(dir)
+    np.savetxt(dir + '/uc.txt', np.array(final_input_uc), fmt='%.3f')
+    np.savetxt(dir + '/vol.txt', final_input_vol, fmt="%.1f")
+    np.savetxt(dir + '/stress.txt', final_input_vol, fmt="%.3f")
+    np.savetxt(dir + '/output.txt', np.array(final_output_rho), fmt="%.3f")
+
+    # Log time
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='logsimp.txt')
+    logging.info(f"Execution time: {execution_time} seconds in SIMP multiprocessing with {num_processor} processors. [{iter*5} samples]")
