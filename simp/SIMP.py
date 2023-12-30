4ee038c1629bd9178636dc065969eeef5e886e1a
kssgarcia <kssgarcia@outlook.com>
2023-12-30 11:26:22 -0500

    fix

--- simp/SIMP.py
+++ simp/SIMP.py
@@ -72,7 +72,7 @@ def optimization(n_elem, volfrac):
         mats[:,2] = Emin+rho**penal*(Emax-Emin)
 
         # System assembly
-        stiff_mat = sparse_assem(els, mats, nodes[:, :3], neq, assem_op, kloc)
+        stiff_mat = sparse_assem(els, mats, neq, assem_op, kloc)
         rhs_vec = ass.loadasem(loads, bc_array, neq)
 
         # System solution
